7f1d3894c9f0744958ceb74f87d02c92e6905652
Jon <jon.a.mccallum@gmail.com>
2020-12-12 17:33:16 +0000

    fixed responsive nav bugs

---
+++ public/scripts/nav-toggle.js
@@ -0,0 +1,37 @@
+const menu = document.getElementById('nav');
+const toggle = document.getElementById('toggle');
+let isMenuOpen = false;
+
+window.onresize = reportWindowSize;
+
+toggle.addEventListener('click', (e) => {
+  if (isMenuOpen) {
+    closeMenu()
+  } else {
+    openMenu()
+  }
+});
+
+function reportWindowSize() {
+  const width = window.innerWidth;
+  console.log(width);
+  if (width > 600) {
+    openMenu()
+  } else if (width < 600) {
+    closeMenu()
+  }
+}
+
+function closeMenu() {
+  toggle.style.transform = 'translateY(0px)'
+  menu.style.opacity = '0';
+  isMenuOpen = false;
+}
+
+function openMenu() {
+  toggle.style.transform = 'translateY(20px)'
+  menu.style.display = 'flex';
+  menu.clientHeight;
+  menu.style.opacity = '1';
+  isMenuOpen = true;
+}
